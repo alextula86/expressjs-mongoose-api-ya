272fca2924426ed5de76bfe44759bc4349ff1560
alextula26 <alex_marc86@mail.ru>
2023-02-18 21:36:54 +0300

    fix connect

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { runDb } from './repositories/db'
+import { runDb } from './repositories/db-mongoose'
 import { blogsRouter } from './routers/blogs-router'
 import { postsRouter } from './routers/posts-router'
 import { commentsRouter } from './routers/comments-router'
